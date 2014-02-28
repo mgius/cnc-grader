3cbc5deef0070324ed8ad6f123ceec67dea8fbca
Mark Gius <mgius7096@gmail.com>
2014-02-27 21:55:25 -0800

    switched to using django-registration

--- cnc_grader/urls.py
+++ cnc_grader/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import login
 
 from cnc_grader.grader_web.views import TeamScoreView, UserSubmissionsView
 admin.autodiscover()
@@ -12,9 +11,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'cnc_grader.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^accounts/login/', login),
-    url(r'^login', login),
+    (r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^submissions/', login_required(UserSubmissionsView.as_view())),
+    url(r'^users/', login_required(UserSubmissionsView.as_view())),
     url(r'^/?$', login_required(TeamScoreView.as_view())),
 )
