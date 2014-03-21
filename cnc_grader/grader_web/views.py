0650eebbef75d7ec2720c67f9333256f33a98374
Mark Gius <mgius7096@gmail.com>
2014-03-20 23:44:13 -0700

    put all program mode back on

--- cnc_grader/grader_web/views.py
+++ cnc_grader/grader_web/views.py
@@ -27,7 +27,7 @@ class TeamScoreView(generic.ListView):
 class SubmissionForm(ModelForm):
     class Meta:
         model = Submission
-        fields = ['file']
+        fields = ['problem', 'file']
 
     def __init__(self, *args, **kwargs):  # pylint: disable=E1002
         self.request = kwargs.pop('request', None)
@@ -38,7 +38,7 @@ class SubmissionForm(ModelForm):
         obj = super(SubmissionForm, self).save(*args, **kwargs)
         if self.request:
             obj.team = self.request.user.team
-            obj.problem = Problem.objects.order_by('-id')[0]
+            # obj.problem = Problem.objects.order_by('-id')[0]
         obj.save()
         return obj
 
